725a24cb866de65229d69216965bd54ac219b1bb
Abel Morales Herrera <abelangelmh@gmail.com>
2019-12-03 18:41:32 -0500

    marketingbancos3

--- routes/sip/index.js
+++ routes/sip/index.js
@@ -6,11 +6,13 @@ const training = require(path.join(__dirname, 'rrhh', 'training.js'));
 const assistance = require(path.join(__dirname, 'rrhh', 'assistance.js'));
 const cubicaje = require(path.join(__dirname, 'logist', 'cubicaje.js'));
 const contabilidad = require(path.join(__dirname, 'contabilidad', 'reporte.js'));
+const marketing = require(path.join(__dirname, 'marketing', 'marketingbancos.js'));
 
 router.use('/rrhh/capacitacion', training);
 router.use('/rrhh/asistencia', assistance);
 router.use('/logist/cubicaje', cubicaje);
 router.use('/contabilidad/reporte', contabilidad);
+router.use('/marketing/pagos', marketing);
 
 router.get('/', (req, res) => {
     res.status(200).json({
