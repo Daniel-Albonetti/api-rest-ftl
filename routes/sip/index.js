aedd006ea3687c1def53fadf756d3d170854f60e
Abel Morales Herrera <abelangelmh@gmail.com>
2019-12-03 16:13:29 -0500

    sip-contabilidad

--- routes/sip/index.js
+++ routes/sip/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const training = require(path.join(__dirname, 'rrhh', 'training.js'));
 const assistance = require(path.join(__dirname, 'rrhh', 'assistance.js'));
 const cubicaje = require(path.join(__dirname, 'logist', 'cubicaje.js'));
+const contabilidad = require(path.join(__dirname, 'contabilidad', 'reporte.js'));
 
 router.use('/rrhh/capacitacion', training);
 router.use('/rrhh/asistencia', assistance);
 router.use('/logist/cubicaje', cubicaje);
+router.use('/contabilidad/reporte', contabilidad);
 
 router.get('/', (req, res) => {
     res.status(200).json({
